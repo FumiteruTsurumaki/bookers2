653cedeb0b9167d47364d03213ceb2a87b906d1c
FumiteruTsurumaki <runprotsuru@gmail.com>
2020-11-27 03:43:56 +0000

    [Finish]トップ画面、サクセスメッセージ、バリデーション追加。css編集。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,25 @@
 class BooksController < ApplicationController
-  def index
-    @books = Book.all
+
+  def top
   end
 
-  def new
+  def index
+    @books = Book.all
     @book = Book.new
   end
 
+  def new
+  end
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render action: :index
+    end
   end
 
   def show
@@ -22,20 +31,25 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book)
+    else
+      render action: :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
   private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 
 end
