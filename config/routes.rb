4913446f7f63d49fcf6562395e0ecd171fa839c8
FumiteruTsurumaki <runprotsuru@gmail.com>
2020-12-12 04:16:18 +0000

    [Check]RSpecによるテスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'home#home'
-  get "home/about"
+  get 'home/about'
   devise_for :users
   resources :books do
     resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
 end
